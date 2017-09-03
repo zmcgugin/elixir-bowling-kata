6dc0d3d5b0e06567a68b8e898e1b1ffc5d24e9d4
Zak <zmcgugin@gmail.com>
2017-09-02 23:34:14 -0400

    inital commit

---
+++ lib/elixir_bowling_kata.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBowlingKata do
+  @moduledoc """
+  Documentation for ElixirBowlingKata.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBowlingKata.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_bowling_kata_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirBowlingKataTest do
+  use ExUnit.Case
+  doctest ElixirBowlingKata
+
+  test "greets the world" do
+    assert ElixirBowlingKata.hello() == :world
+  end
+end

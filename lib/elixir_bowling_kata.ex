44b19343b0d6ccc1365b0b4238e02ec18348ba61
Zak <zmcgugin@gmail.com>
2017-09-03 00:25:59 -0400

    adding test to sum frames

--- lib/elixir_bowling_kata.ex
+++ lib/elixir_bowling_kata.ex
@@ -1,6 +1,22 @@
 defmodule ElixirBowlingKata do
 
   def bowl(frames) do
+    sum_frames(frames)
+  end
+
+  def sum_frames([h|t]) do
+    sum_frame(h) + sum_frames(t)
+  end
+
+  def sum_frames([]) do
+    0
+  end
+
+  def sum_frame([h|t]) do
+    h + sum_frame(t)
+  end
+
+  def sum_frame([]) do
     0
   end
 end
--- test/elixir_bowling_kata_test.exs
+++ test/elixir_bowling_kata_test.exs
@@ -2,8 +2,14 @@ defmodule ElixirBowlingKataTest do
   use ExUnit.Case
   doctest ElixirBowlingKata
 
-  test "bowling all gutters results in a score of 0" do
-    frames = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+  test "bowl all gutters results in a score of 0" do
+    frames = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
     assert ElixirBowlingKata.bowl(frames) == 0
   end
+
+  test "bowl should sum frames for a total score" do
+    frames = [[5,3], [2,7]]
+    assert ElixirBowlingKata.bowl(frames) == 17
+  end
+
 end
